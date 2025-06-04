a6d80465b3ef21f454bf478e1126e2839bd99967
byxor <brandnewbyxor@gmail.com>
2025-06-05 00:04:57 +0100

    Fix missing files

---
+++ guides/common-functionality.js
@@ -0,0 +1,28 @@
+function githubLogo() {
+    return `<img
+      src="/assets/github/github-mark.svg"
+      alt="GitHub Logo"
+      style="height: 1em; vertical-align: -0.125em;"
+    >`;
+}
+
+function constrainedImage(src, alt="", classes="", fullscreen=true) {
+	if (fullscreen) {
+		classes += "constrained-image-pointer";
+	}
+	const html = `<img
+		src="${src}"
+		alt="${alt}"
+		${fullscreen ? `onclick="this.requestFullscreen({navigationUI: 'show'})"` : ""} class="constrained-image ${classes}"
+		draggable="false"
+	>
+	</img>`
+	return html;
+}
+
+function title(title) {
+    return `<div class="header">
+        <a href="/" class="home-link">⬅ Back</a>
+        <h1>${title}</h1>
+    </div>`;
+}
